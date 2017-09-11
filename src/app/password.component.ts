05e8c4e5cb2a39205c9d218721be15f9269777d4
Joanna <joannag6@gmail.com>
2017-09-11 10:37:35 +1000

    Added password files

---
+++ src/app/password.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import '@angular/material';
+
+@Component({
+  selector: 'password',
+  templateUrl: 'password.component.html',
+  styleUrls: ['password.component.css']
+})
+export class PasswordComponent {
+  title = 'Resource To Help Deaf Or Hard-Of-Hearing (DHH) Students And Their Parents Decide On A Primary School.';
+}
